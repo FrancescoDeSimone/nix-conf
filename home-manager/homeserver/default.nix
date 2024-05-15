152041ff130e8223c88a4b7809a9261ae0c8fb39
fdesi <francesco.desimone@canonical.com>
2024-05-15 09:57:29 +0200

    fix homeserver homemanager

--- home-manager/homeserver/default.nix
+++ home-manager/homeserver/default.nix
@@ -1,13 +1,1 @@
-{ pkgs, lib, inputs, config, ... }: {
-  imports = [ ./zsh.nix ./htop.nix ./tmux.nix ./starship.nix ];
-  home.packages = with pkgs; [
-    home-manager
-    neovim
-    git
-    ripgrep
-    fd
-    fzf
-    unstable.rsync
-  ];
-  xdg.enable = true;
-}
+{ pkgs, ... }: { home.packages = with pkgs; [ ]; }
