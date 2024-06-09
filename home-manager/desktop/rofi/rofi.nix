6de8da862a2d095941a48bfb61a22e7375fdc49c
fdesi <francesco.desimone@canonical.com>
2024-06-10 00:23:33 +0200

    update rofi

--- home-manager/desktop/rofi/rofi.nix
+++ home-manager/desktop/rofi/rofi.nix
@@ -4,8 +4,6 @@
     package = pkgs.rofi-wayland.override {
       plugins = with pkgs; [
         rofi-calc
-        rofi-bluetooth
-        rofi-mpd
         rofi-top
         rofi-vpn
       ];
@@ -13,7 +11,8 @@
     pass.enable = true;
     pass.package = pkgs.rofi-pass-wayland;
     pass.stores = [ "$HOME/.config/.password-store" ];
-    extraConfig.modi = "drun,ssh";
+    extraConfig.modi = "drun,ssh,calc,top,filebrowser";
+    theme = "~/.nix-profile/share/rofi/themes/Arc-Dark.rasi";
     catppuccin.enable = false;
     terminal = "${pkgs.foot}/bin/foot";
   };
--- home-manager/phoenix.nix
+++ home-manager/phoenix.nix
@@ -8,11 +8,16 @@
   catppuccin.enable = true;
   catppuccin.flavor = "mocha";
   nixpkgs = {
-    overlays = [
-      outputs.overlays.additions
-      outputs.overlays.modifications
-      outputs.overlays.unstable-packages
-    ];
+    overlays = [(final: prev: {
+      rofi-calc = prev.rofi-calc.override {rofi-unwrapped = prev.rofi-wayland-unwrapped; };
+      rofi-top = prev.rofi-top.override {rofi-unwrapped = prev.rofi-wayland-unwrapped; };
+      rofi-vpn = prev.rofi-vpn.override {rofi-unwrapped = prev.rofi-wayland-unwrapped; };
+    })
+    outputs.overlays.additions
+    outputs.overlays.modifications
+    outputs.overlays.unstable-packages
+  ];
+
     config = {
       allowUnfree = true;
       allowUnfreePredicate = _: true;
