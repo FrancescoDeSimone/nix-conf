f0eda7e9c5b28adcce4e684b65cc8c953a0da479
fdesi <francesco.desimone@canonical.com>
2024-11-03 19:47:29 +0100

    formating

--- nixos/applications.nix
+++ nixos/applications.nix
@@ -1,10 +1,9 @@
-{ pkgs, ... }: {
+{pkgs, ...}: {
   environment.sessionVariables = {
     XDG_CONFIG_HOME = "$HOME/etc";
     XDG_DATA_HOME = "$HOME/var/lib";
     XDG_CACHE_HOME = "$HOME/var/cache";
   };
-
-  environment.systemPackages = with pkgs; [ ];
-
+  environment.systemPackages = with pkgs; [
+  ];
 }
--- nixos/pegasus/filesystem.nix
+++ nixos/pegasus/filesystem.nix
@@ -1,16 +1,13 @@
-{ config, pkgs, ... }:
-
 {
   fileSystems."/data" = {
     device = "/dev/pool/data";
     fsType = "ext4";
-    options = ["nofail" "defaults" "noatime" "data=writeback" "barrier=0" "nobh" "errors=continue" "commit=100" ];
+    options = ["nofail" "defaults" "noatime" "data=writeback" "barrier=0" "nobh" "errors=continue" "commit=100"];
   };
 
   fileSystems."/nextcloud" = {
     device = "/dev/sda2";
     fsType = "ext4";
-    options = ["nofail" "defaults" "noatime" "data=writeback" "barrier=0" "nobh" "errors=continue" "commit=100" ];
+    options = ["nofail" "defaults" "noatime" "data=writeback" "barrier=0" "nobh" "errors=continue" "commit=100"];
   };
 }
-
