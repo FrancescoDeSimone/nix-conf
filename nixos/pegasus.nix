75ed3520a1d1590ad71f6bbe1673a610613f8e5c
fdesi <francesco.desimone@canonical.com>
2024-11-03 19:50:01 +0100

    change format

--- nixos/pegasus.nix
+++ nixos/pegasus.nix
@@ -1,18 +1,19 @@
-{ inputs, outputs, lib, config, unstable, pkgs, ... }: {
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
   imports = [
     ./pegasus/hardware-configuration.nix
     ./pegasus/services.nix
     ./pegasus/applications.nix
-    #./pegasus/disks.nix
     ./pegasus/filesystem.nix
-    ./pegasus/nextcloud.nix
-    ./pegasus/git.nix
     ./pegasus/cron.nix
     ./applications.nix
-    ./pegasus/docker.nix
     ./general/lxd.nix
-    ./pegasus/homepage.nix
-    #inputs.home-manager.nixosModules.home-manager
   ];
 
   age.secrets = {
@@ -21,17 +22,21 @@
     };
   };
   nixpkgs = {
+    config.permittedInsecurePackages = [
+      "python3.11-django-3.1.14"
+    ];
     overlays = [
       outputs.overlays.additions
       outputs.overlays.modifications
       outputs.overlays.unstable-packages
     ];
-    config = { allowUnfree = true; };
+    config = {allowUnfree = true;};
   };
-  system.autoUpgrade = { enable = true; };
+  system.autoUpgrade = {enable = true;};
   nix = {
-    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
-    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}")
+    registry = lib.mapAttrs (_: value: {flake = value;}) inputs;
+    nixPath =
+      lib.mapAttrsToList (key: value: "${key}=${value.to.path}")
       config.nix.registry;
     optimise.automatic = true;
     gc = {
@@ -45,14 +50,7 @@
     };
   };
 
-  #home-manager = {
-  #  extraSpecialArgs = { inherit unstable inputs outputs; };
-  #  users = { thinkcentre = import ../home-manager/pegasus.nix; };
-  #};
-
-  boot.kernelParams = [ "amd_iommu=off" ];
-  boot.initrd.kernelModules = [ "amdgpu" ];
-  services.xserver.videoDrivers = [ "amdgpu" ];
+  services.xserver.videoDrivers = ["amdgpu"];
   hardware.opengl = {
     enable = true;
     extraPackages = with pkgs; [
@@ -62,45 +60,54 @@
       libvdpau-va-gl
     ];
   };
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.blacklistedKernelModules = [ "bluetooth" "snd_hda_intel" ];
+  boot = {
+    kernelParams = ["amd_iommu=off"];
+    initrd.kernelModules = ["amdgpu"];
+    kernelPackages = pkgs.linuxPackages_latest;
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+    blacklistedKernelModules = ["bluetooth" "snd_hda_intel"];
+  };
 
-  networking.hostName = "pegasus";
-  networking.networkmanager.enable = true;
-  systemd.coredump.enable = false;
-  services.journald.extraConfig = "Storage=volatile";
-  services.journald.forwardToSyslog = false;
-  systemd.services.systemd-journal-flush.enable = false;
-  services.logrotate.enable = false;
-  systemd.targets.sleep.enable = false;
-  systemd.targets.suspend.enable = false;
-  systemd.targets.hibernate.enable = false;
-  systemd.targets.hybrid-sleep.enable = false;
+  networking = {
+    hostName = "pegasus";
+    networkmanager.enable = true;
+  };
+  systemd = {
+    coredump.enable = false;
+    services.systemd-journal-flush.enable = false;
+    targets.sleep.enable = false;
+    targets.suspend.enable = false;
+    targets.hibernate.enable = false;
+    targets.hybrid-sleep.enable = false;
+  };
+
   security.tpm2.enable = false;
   hardware.cpu.amd.updateMicrocode = true;
   programs.zsh.enable = true;
-  users.defaultUserShell = pkgs.zsh;
 
   time.timeZone = "Europe/Rome";
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "it_IT.UTF-8";
-    LC_IDENTIFICATION = "it_IT.UTF-8";
-    LC_MEASUREMENT = "it_IT.UTF-8";
-    LC_MONETARY = "it_IT.UTF-8";
-    LC_NAME = "it_IT.UTF-8";
-    LC_NUMERIC = "it_IT.UTF-8";
-    LC_PAPER = "it_IT.UTF-8";
-    LC_TELEPHONE = "it_IT.UTF-8";
-    LC_TIME = "it_IT.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "it_IT.UTF-8";
+      LC_IDENTIFICATION = "it_IT.UTF-8";
+      LC_MEASUREMENT = "it_IT.UTF-8";
+      LC_MONETARY = "it_IT.UTF-8";
+      LC_NAME = "it_IT.UTF-8";
+      LC_NUMERIC = "it_IT.UTF-8";
+      LC_PAPER = "it_IT.UTF-8";
+      LC_TELEPHONE = "it_IT.UTF-8";
+      LC_TIME = "it_IT.UTF-8";
+    };
   };
-  users.users.thinkcentre = {
-    isNormalUser = true;
-    description = "thinkcentre";
-    extraGroups = [ "networkmanager" "wheel" "lxd" "docker" ];
+  users = {
+    defaultUserShell = pkgs.zsh;
+    users.thinkcentre = {
+      isNormalUser = true;
+      description = "thinkcentre";
+      extraGroups = ["networkmanager" "wheel" "lxd" "docker"];
+    };
   };
-  services.openssh.enable = true;
   system.stateVersion = "24.05"; # Did you read the comment?
 }
