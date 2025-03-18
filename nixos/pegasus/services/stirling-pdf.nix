a9f44a9ef2e2ec427675a79f2f3c505e8dad3b9d
fdesi <francesco.desimone@canonical.com>
2025-03-18 12:43:00 +0100

    use stable for stirling-pdf

--- nixos/pegasus/services/stirling-pdf.nix
+++ nixos/pegasus/services/stirling-pdf.nix
@@ -3,13 +3,13 @@
   inputs,
   ...
 }: {
-  disabledModules = ["services/web-apps/stirling-pdf.nix"];
-  imports = [
-    "${inputs.nixpkgs-unstable}/nixos/modules/services/web-apps/stirling-pdf.nix"
-  ];
+  #disabledModules = ["services/web-apps/stirling-pdf.nix"];
+  #imports = [
+  #  "${inputs.nixpkgs-unstable}/nixos/modules/services/web-apps/stirling-pdf.nix"
+  #];
   services.stirling-pdf = {
     enable = true;
-    package = pkgs.unstable.stirling-pdf;
+    #package = pkgs.unstable.stirling-pdf;
     environment = {
       INSTALL_BOOK_AND_ADVANCED_HTML_OPS = "true";
       SERVER_PORT = 8080;
