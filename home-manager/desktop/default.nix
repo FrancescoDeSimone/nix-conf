d910b9af6e5ea096bb7584be3195875d8afcb64e
fdesi <francesco.desimone@canonical.com>
2025-09-08 10:17:35 +0200

    update nerd-fonts

--- home-manager/desktop/default.nix
+++ home-manager/desktop/default.nix
@@ -1,21 +1,26 @@
 {pkgs, ...}: {
   imports = [./dunst.nix ./firefox.nix ./rofi/rofi.nix ./pass.nix ./script.nix];
-  home.packages = with pkgs; [
-    nerdfonts
-    fira-code-nerdfont
-    playerctl
-    telegram-desktop
-    fira-code-symbols
-    xdragon
-    networkmanagerapplet
-    fira-code
-    sshuttle
-    supersonic
-    pavucontrol
-    # unstable.freetube
-    obsidian
-    unstable.finamp
-  ];
+  home.packages = with pkgs;
+    [
+      # nerdfonts
+      # nerd-fonts.fira-code
+      # nerd-fonts.fira-mono
+      # nerd-fonts.ubuntu
+      fira-code-nerdfont
+      playerctl
+      telegram-desktop
+      fira-code-symbols
+      xdragon
+      networkmanagerapplet
+      fira-code
+      sshuttle
+      supersonic
+      pavucontrol
+      # unstable.freetube
+      obsidian
+      unstable.finamp
+    ]
+    ++ builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
   fonts.fontconfig.enable = true;
   systemd.user = {
     enable = true;
