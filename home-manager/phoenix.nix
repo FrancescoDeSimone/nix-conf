5c9aabcb5fc1288cad2603e365ed78c6bcaf7809
fdesi <francesco.desimone@canonical.com>
2025-12-08 14:40:04 +0100

    add specific sway config for phoenix

--- home-manager/phoenix.nix
+++ home-manager/phoenix.nix
@@ -6,16 +6,76 @@
   # You can import other home-manager modules here
   imports = [./desktop/default.nix ./cli/default.nix ./desktop/wayland/default.nix];
 
-  home.packages = with pkgs; [brightnessctl jellyfin-tui yq jq];
+  home.packages = with pkgs; [jellyfin-tui yq jq ayugram-desktop];
+
+  xdg.dataFile."wayland-sessions/sway-nvidia.desktop".text = ''
+    [Desktop Entry]
+    Name=Sway (Nvidia)
+    Comment=An i3-compatible Wayland compositor
+    Exec=env WLR_DRM_DEVICES=/dev/dri/card1:/dev/dri/card2 WLR_NO_HARDWARE_CURSORS=1 /usr/bin/sway --unsupported-gpu
+    Type=Application
+  '';
+  wayland.windowManager = {
+    sway = {
+      package = null;
+      checkConfig = false;
+      enable = true;
+      config = {
+        workspaceOutputAssign = [
+          {
+            workspace = "1";
+            output = "DP-1";
+          }
+          {
+            workspace = "2";
+            output = "DP-1";
+          }
+          {
+            workspace = "3";
+            output = "DP-1";
+          }
+          {
+            workspace = "4";
+            output = "DP-1";
+          }
+          {
+            workspace = "5";
+            output = "DP-1";
+          }
+          {
+            workspace = "6";
+            output = "HDMI-A-1";
+          }
+          {
+            workspace = "7";
+            output = "HDMI-A-1";
+          }
+          {
+            workspace = "8";
+            output = "HDMI-A-1";
+          }
+          {
+            workspace = "9";
+            output = "HDMI-A-1";
+          }
+          {
+            workspace = "10";
+            output = "eDP-1";
+          }
+        ];
+      };
+    };
+  };
+  programs.swaylock.package = null;
 
   catppuccin.enable = true;
   catppuccin.flavor = "mocha";
   nixpkgs = {
     overlays = [
       (final: prev: {
-        rofi-calc = prev.rofi-calc.override {rofi-unwrapped = prev.rofi-wayland-unwrapped;};
-        rofi-top = prev.rofi-top.override {rofi-unwrapped = prev.rofi-wayland-unwrapped;};
-        rofi-vpn = prev.rofi-vpn.override {rofi-unwrapped = prev.rofi-wayland-unwrapped;};
+        rofi-calc = prev.rofi-calc.override {rofi-unwrapped = prev.rofi-unwrapped;};
+        rofi-top = prev.rofi-top.override {rofi-unwrapped = prev.rofi-unwrapped;};
+        rofi-vpn = prev.rofi-vpn.override {rofi-unwrapped = prev.rofi-unwrapped;};
       })
       outputs.overlays.additions
       outputs.overlays.modifications
