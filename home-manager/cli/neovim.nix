9900f8e0b0fd5b6e86da6dbaaa0a3f36e1a2ec3c
fdesi <francesco.desimone@canonical.com>
2025-12-21 17:14:08 +0100

    add optionals in neovim

--- home-manager/cli/neovim.nix
+++ home-manager/cli/neovim.nix
@@ -1,43 +1,52 @@
 {
   pkgs,
   lib,
+  config,
   inputs,
   ...
 }: let
-  # kickstart-nvim = pkgs.fetchFromGitHub {
-  #   owner = "FrancescoDeSimone";
-  #   repo = "kickstart.nvim";
-  #   rev = "21f6d8dc8531bf7ce9937b02ee29488dd4bf3a9e";
-  #   sha256 = "sha256-IEDSDhquU4moWcK5O5T05HjtJk8orCnY0xcrtRwk804=";
-  # };
+  cfg = config.modules.editors.neovim;
 in {
-  home.packages = with pkgs; [
-    neovim
-    nodejs_24
-    python315
-    go
-    gcc
-    gcc
-    pkg-config
-    hadolint
-    clj-kondo
-    markdownlint-cli
-    vale
-    ruby
-    tflint
-    stylua
-    black
-    prettierd
-    shellcheck
-    rustfmt
-    alejandra
-    git
-    ripgrep
-    fd
-    unzip
-  ];
-  xdg.configFile."nvim" = {
-    source = inputs.kickstart-nvim;
-    recursive = true;
+  options.modules.editors.neovim = {
+    extras = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Put this to false if you want install stuff manually";
+    };
+  };
+  config = {
+    home.packages = with pkgs;
+      [
+        git
+        ripgrep
+        fd
+        unzip
+        alejandra
+        nodejs_24
+        python315
+        go
+        hadolint
+        clj-kondo
+        markdownlint-cli
+        vale
+        ruby
+        tflint
+        stylua
+        black
+        prettierd
+        shellcheck
+      ]
+      ++ lib.optionals cfg.extras [
+        neovim
+        # added in a specific layer
+        # rustfmt
+        # gcc
+        # pkg-config
+      ];
+
+    xdg.configFile."nvim" = {
+      source = inputs.kickstart-nvim;
+      recursive = true;
+    };
   };
 }
