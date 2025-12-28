dded6dd880050d569d21f92c91bddb63342fe946
fdesi <desimonefr30@gmail.com>
2025-12-28 21:38:00 +0100

    edit pass

--- home-manager/desktop/pass.nix
+++ home-manager/desktop/pass.nix
@@ -1,7 +1,20 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  inputs,
+  private,
+  ...
+}: {
   programs.password-store = {
     enable = true;
     package = pkgs.pass.withExtensions (exts: [exts.pass-otp]);
-    settings = {PASSWORD_STORE_DIR = "$HOME/.config/.password-store";};
+    settings = {
+      PASSWORD_STORE_DIR = "${config.home.homeDirectory}/.config/.password-store";
+    };
+  };
+
+  home.file.".config/.password-store" = {
+    source = inputs.private.inputs.password-store-repo;
+    recursive = true;
   };
 }
