b4025ebe53f67ae501b9788c22cdb66e46feb720
fdesi <francesco.desimone@canonical.com>
2026-03-01 19:37:36 +0100

    change output port

--- home-manager/phoenix.nix
+++ home-manager/phoenix.nix
@@ -24,23 +24,23 @@
         workspaceOutputAssign = [
           {
             workspace = "1";
-            output = "DP-1";
+            output = "DP-2";
           }
           {
             workspace = "2";
-            output = "DP-1";
+            output = "DP-2";
           }
           {
             workspace = "3";
-            output = "DP-1";
+            output = "DP-2";
           }
           {
             workspace = "4";
-            output = "DP-1";
+            output = "DP-2";
           }
           {
             workspace = "5";
-            output = "DP-1";
+            output = "DP-2";
           }
           {
             workspace = "6";
