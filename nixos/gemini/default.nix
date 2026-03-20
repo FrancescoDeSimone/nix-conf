79c70e524117aefa2c2eea0877f73f3c1e51f372
fdesi <desimonefr30@gmail.com>
2026-03-20 19:42:02 +0100

    gemini: remove hardware.nix duplicate imports

--- nixos/gemini/default.nix
+++ nixos/gemini/default.nix
@@ -19,19 +19,5 @@
     inputs.catppuccin.nixosModules.catppuccin
   ];
 
-  nix = {
-    settings = {
-      experimental-features = [ "nix-command" "flakes" ];
-      auto-optimise-store = true;
-    };
-    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
-    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
-  };
-
   nixpkgs.hostPlatform = "x86_64-linux";
-  time.timeZone = "Europe/Rome";
-  i18n.defaultLocale = "en_US.UTF-8";
-  system.stateVersion = "25.11";
-
-  networking.networkmanager.enable = true;
 }
