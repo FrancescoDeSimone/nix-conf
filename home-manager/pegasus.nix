2b9f3ce046d5f8a829b498cace66d23098d20d5a
fdesi <francesco.desimone@canonical.com>
2026-04-06 22:47:30 +0200

    add OSC52 for ssh

--- home-manager/pegasus.nix
+++ home-manager/pegasus.nix
@@ -7,4 +7,20 @@
     homeDirectory = "/home/thinkcentre";
     stateVersion = "25.11";
   };
+
+  # Headless server: no Wayland/X11 clipboard provider available.
+  # Use OSC 52 so copy/paste works over SSH via terminal escape sequences.
+  xdg.configFile."nvim/after/plugin/clipboard.lua".text = ''
+    vim.g.clipboard = {
+      name = 'OSC 52',
+      copy = {
+        ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+      },
+      paste = {
+        ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+      },
+    }
+  '';
 }
