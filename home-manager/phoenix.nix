a00e9ad456e8ac2ee046f5b6fd949360275d1ff0
fdesi <francesco.desimone@canonical.com>
2026-08-03 12:58:08 +0200

    change display output

--- home-manager/phoenix.nix
+++ home-manager/phoenix.nix
@@ -21,23 +21,23 @@
         workspaceOutputAssign = [
           {
             workspace = "1";
-            output = "DP-2";
+            output = "DP-1";
           }
           {
             workspace = "2";
-            output = "DP-2";
+            output = "DP-1";
           }
           {
             workspace = "3";
-            output = "DP-2";
+            output = "DP-1";
           }
           {
             workspace = "4";
-            output = "DP-2";
+            output = "DP-1";
           }
           {
             workspace = "5";
-            output = "DP-2";
+            output = "DP-1";
           }
           {
             workspace = "6";
