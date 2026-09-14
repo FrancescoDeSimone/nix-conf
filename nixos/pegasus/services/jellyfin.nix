0620f0edf971cbc472d2f90ca78c11ff7678ca20
fdesi <francesco.desimone@canonical.com>
2026-09-14 18:07:18 +0200

    versioned jellyclip tags

--- nixos/pegasus/services/jellyfin.nix
+++ nixos/pegasus/services/jellyfin.nix
@@ -8,7 +8,7 @@
       jellyfin-web = pkgs.unstable.jellyfin-web.overrideAttrs (_: _: {
         installPhase = ''
           runHook preInstall
-          sed -i "s#</body>#<!-- JellyClip-Ui:start -->\n<link rel=\"stylesheet\" href=\"configurationpage?name=jellyclip.css\">\n<script src=\"configurationpage?name=jellyclip.js\"></script>\n<!-- JellyClip-Ui:end -->\n</body>#" dist/index.html
+          sed -i "s#</body>#<!-- JellyClip-Ui:start -->\n<link rel=\"stylesheet\" href=\"configurationpage?name=jellyclip.css&v=0.2.0.0\">\n<script src=\"configurationpage?name=jellyclip.js&v=0.2.0.0\"></script>\n<!-- JellyClip-Ui:end -->\n</body>#" dist/index.html
           mkdir -p $out/share
           cp -a dist $out/share/jellyfin-web
           runHook postInstall
